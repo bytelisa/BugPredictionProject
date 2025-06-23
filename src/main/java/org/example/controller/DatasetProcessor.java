87257d39ec749c2e76512b71532848175c162504
bytelisa <elisa.marzioli2002@gmail.com>
2025-06-23 14:54:24 +0200

    DatasetProcessor

---
+++ src/main/java/org/example/controller/DatasetProcessor.java
@@ -0,0 +1,10 @@
+package org.example.controller;
+
+public class DatasetProcessor {
+
+    /** Class responsibility:
+     * processing data received by the other controllers
+     * printing the final csv file
+     */
+
+}
