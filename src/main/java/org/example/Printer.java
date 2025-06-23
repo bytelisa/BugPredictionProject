f524cf42d3553de0a21b808f363fd6969a4c239a
bytelisa <elisa.marzioli2002@gmail.com>
2025-06-23 13:00:37 +0200

    printer class to reduce smells

---
+++ src/main/java/org/example/Printer.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+
+public class Printer {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+
+    private Printer(){}
+
+    //stampa
+    public static void print(String message){
+        System.out.print(message);
+    }
+
+    //stampa e va a capo
+    public static void println(String message){
+        System.out.println(message);
+    }
+
+    //stampa messaggio di errore
+    public static void errorPrint(String message) {
+        System.out.println(ANSI_RED + message + ANSI_RESET);
+    }
+}
