7b473dd039b1beb81748f501de14c88c50423de2
bytelisa <elisa.marzioli2002@gmail.com>
2025-06-30 18:45:00 +0200

    main update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
 import org.example.controller.DatasetProcessor;
-import org.example.controller.GitController;
-import org.example.controller.JiraController;
-import org.example.controller.ReleaseInfoExtractor;
 import org.example.util.Printer;
 import org.json.JSONException;
 
@@ -14,7 +11,7 @@ public class Main {
 
         try {
             DatasetProcessor datasetProcessor = new DatasetProcessor();
-            datasetProcessor.start();
+            datasetProcessor.extractData();
         } catch (IOException | JSONException e) {
             Printer.errorPrint("Cannot extract release data.");
         }
